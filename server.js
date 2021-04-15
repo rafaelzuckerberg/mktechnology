f4b3016c780cf67b7445241a9c20bed94b8bb472
rafaelzuckerberg <contatorafaelmoura@gmail.com>
2021-04-15 13:27:25 -0300

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/index.html'));
 
-app.get('/*', (req, res) => 
+app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname + '/dist/index.html')),
 );
 

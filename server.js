d3a6582b4f7aa11cb63c0025c6bdd7799bdaac6b
rafaelzuckerberg <contatorafaelmoura@gmail.com>
2021-04-15 13:14:26 -0300

    update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/mktechnology'));
+app.use(express.static(__dirname + '/dist/index.html'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', { root: 'dist/mktechnology/' }),
+    res.sendFile(path.join(__dirname + '/dist/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
